0b6e4077a1c6df4cc12eb21ae21de456e83e0eff
bimarshak7 <khanalbimarsha7@gmail.com>
2023-06-30 10:15:54 +0545

    Vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { config } from 'dotenv'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-})
+export default ({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    return defineConfig({
+        plugins: [react()],
+        server: {
+            host: env.BACKEND_HOST || 'localhost',
+            port: env.PORT,
+            proxy: {
+                '/api': {
+                    target: `http://${env.BACKEND_HOST || 'localhost'}:${
+                        env.BACKEND_PORT || 3000
+                    }`,
+                },
+            },
+        },
+    })
+}
