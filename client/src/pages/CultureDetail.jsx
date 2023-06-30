db1a84a19dfd5623f60589f06a34dcf00e45c953
needl3 <57314527+needl3@users.noreply.github.com>
2023-07-01 03:13:19 +0545

    Style changes for culture detail page

--- client/src/pages/CultureDetail.jsx
+++ client/src/pages/CultureDetail.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import { BiSolidUpArrow, BiSolidDownArrow } from 'react-icons/bi'
 
 import Attire from '../components/Attire'
 import Map from './Map'
 import { cultureList } from '../../data/culture'
 
 const CultureDetail = () => {
-    const [collapse, setCollapse] = useState([true, false, false, false, false, false])
+    const [collapse, setCollapse] = useState([true, true, true, false, false, false])
     let { id } = useParams()
 
     const updateCol = idx => {
@@ -25,37 +26,37 @@ const CultureDetail = () => {
             </div>
         )
     return (
-        <div className="flex flex-col h-full w-full justify-between">
-            <div className="relative flex-shrink-0 h-1/3 overflow-hidden">
-                <h1 className="text-2xl font-bold text-center">
+        <div className="flex flex-col h-full w-full justify-start gap-y-4">
+            <div className="relative flex-shrink-0 h-2/5 overflow-hidden">
+                <h1 className="text-3xl font-bold text-center pb-4">
                     {cultureList[id]['name']}
                 </h1>
                 <div className="section">
                     <h2
-                        className="text-xl cursor-pointer"
+                        className="cursor-pointer flex items-center justify-between px-3 py-1 shadow-md"
                         onClick={e => updateCol(0)}
                     >
-                        Map {collapse[0] ? '▴' : '▾'}
+                        <span className='text-2xl'>Map</span> {collapse[0] ? <BiSolidUpArrow size='1.5em'/> : <BiSolidDownArrow size='1.5em'/>}
                     </h2>
                     {collapse[0] && <Map toHigh={cultureList[id]['origin']} />}
                 </div>
             </div>
-            <div className="border border-black border-x-4 border-t-4 flex flex-col gap-y-2 rounded-md transition duration-300 p-2 overflow-scroll h-fit">
-                <div className="flex flex-col">
+            <div className="flex flex-col gap-y-2 rounded-md transition duration-300 overflow-scroll h-fit">
+                <div className="flex flex-col gap-y-4">
                     <div className="section flex-shrink-0">
                         <h2
-                            className="text-xl cursor-pointer"
+                            className="cursor-pointer flex items-center justify-between px-3 py-1 shadow-md"
                             onClick={e => updateCol(1)}
                         >
-                            Description {collapse[1] ? '▴' : '▾'}
+                            <span className='text-2xl'>Description</span> {collapse[1] ? <BiSolidUpArrow size='1.5em'/> : <BiSolidDownArrow size='1.5em'/>}
                         </h2>
                         {collapse[1] && <p>{cultureList[id]['description']}</p>}                    </div>
                     <div className="section">
                         <h2
-                            className="text-xl cursor-pointer"
-                            onClick={e => updateCol(1)}
+                            className="cursor-pointer flex items-center justify-between px-3 py-1 shadow-md"
+                            onClick={e => updateCol(2)}
                         >
-                            Traditional Attire {collapse[2] ? '▴' : '▾'}
+                            <span className='text-2xl'>Traditional Attire</span> {collapse[2] ? <BiSolidUpArrow size='1.5em'/> : <BiSolidDownArrow size='1.5em'/>}
                         </h2>
                         {collapse[2] && (
                             <Attire culture={cultureList[id]['name']} />
@@ -63,10 +64,10 @@ const CultureDetail = () => {
                     </div>
                     <div className="section">
                         <h2
-                            className="text-xl cursor-pointer"
+                            className="cursor-pointer flex items-center justify-between px-3 py-1 shadow-md"
                             onClick={e => updateCol(3)}
                         >
-                            Festivals {collapse[3] ? '▴' : '▾'}
+                            <span className='text-2xl'>Festivals</span> {collapse[3] ? <BiSolidUpArrow size='1.5em'/> : <BiSolidDownArrow size='1.5em'/>}
                         </h2>
                         {collapse[3] && (
                             <p>
@@ -80,10 +81,10 @@ const CultureDetail = () => {
                     </div>
                     <div className="section">
                         <h2
-                            className="text-xl cursor-pointer"
+                            className="cursor-pointer flex items-center justify-between px-3 py-1 shadow-md"
                             onClick={e => updateCol(4)}
                         >
-                            Food {collapse[4] ? '▴' : '▾'}
+                            <span className='text-2xl'>Foods</span> {collapse[4] ? <BiSolidUpArrow size='1.5em'/> : <BiSolidDownArrow size='1.5em'/>}
                         </h2>
                         {collapse[4] && (
                             <p>
