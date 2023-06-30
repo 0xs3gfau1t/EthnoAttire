a337a56e30cd73c78234b6e54c1cad663fe22bd3
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2023-06-30 18:17:46 +0545

    Consistent UI

--- client/src/pages/Image.jsx
+++ client/src/pages/Image.jsx
@@ -1,6 +1,6 @@
 import UploadCard from '../components/UploadCard'
 import { AiOutlineCloudUpload } from 'react-icons/ai'
-import { useEffect, useRef, useState } from 'react'
+import { useRef, useState } from 'react'
 import { GrClear } from 'react-icons/gr'
 export default function Image() {
     const [targetImage, setTargetImage] = useState(null)
@@ -14,30 +14,24 @@ export default function Image() {
         const reader = new FileReader()
         reader.readAsDataURL(e.target.files[0])
         reader.onload = r => {
-            setTargetImage(r?.target?.result )
+            setTargetImage(r?.target?.result)
             setImageFile(e.target.files[0])
         }
     }
     function handleSubmit() {
         const data = new FormData()
         data.append('img', imageFile)
-        fetch(
-            '/api/image',
-            {
-                method: 'POST',
-                body: data
-            }
-        )
+        fetch('/api/image', {
+            method: 'POST',
+            body: data,
+        })
             .then(r => r.json())
             .then(r => {
-                const tempBbox = []
-                const tempPredictedClass = []
-                for (let i of r.frame){
-                    tempBbox.push(i.box)
-                    tempPredictedClass.push(i.name)
-                }
-                setPredictedClasses(tempPredictedClass)
-                setBbox(tempBbox)
+                /* const tempBbox = [] */
+                /* for (let i of r.frame) { */
+                /*     tempBbox.push(i.box) */
+                /* } */
+                setBbox(r.frame.map(i => i.box))
                 setDetected(true)
             })
             .catch(e => {
@@ -51,10 +45,10 @@ export default function Image() {
 
     const [bounding, setBounding] = useState(null)
 
-    function bounds(){
+    function bounds() {
         const p = parentRef.current?.getBoundingClientRect()
         const i = imageRef.current?.getBoundingClientRect()
-        
+
         if (!p || !i) setBounding(null)
         else
             setBounding({
@@ -63,50 +57,74 @@ export default function Image() {
                 top: i.top - p.top,
                 bottom: p.bottom - i.bottom,
                 width: i.width,
-                height: i.height
+                height: i.height,
             })
     }
     return (
         <>
             {targetImage === null ? (
                 <UploadCard
-                    handleSubmit={handleSubmit}
                     handleChange={handleChange}
+                    image="assets/imageHolder.svg"
+                    accept="image/*"
                 />
             ) : (
-                <div className='flex flex-col'>
-                    <div className="relative mt-10" ref={parentRef}>
-                        <img src={targetImage} className="h-auto w-[20rem] inset-0 m-auto" onLoad={bounds} ref={imageRef}/> 
-                        {
-                            detected &&
+                <div className="h-full w-full flex flex-col justify-center gap-2">
+                    <div className="h-1/2 relative" ref={parentRef}>
+                        <img
+                            src={targetImage}
+                            className="h-full absolute m-auto inset-0"
+                            onLoad={bounds}
+                            ref={imageRef}
+                        />
+                        {detected && (
                             <ul>
-                                {
-                                    bbox.map(box => {
-                                        const a = <li className='border border-black absolute'
-                                        style={{
-                                            left: `${bounding.left + box[0] * bounding.width}px`,
-                                            top: `${bounding.top + box[1] * bounding.height}px`,
-                                            right: `${bounding.right + (1-box[2]) * bounding.width}px`,
-                                            bottom: `${bounding.bottom + (1-box[3]) * bounding.height}px`
-                                        }} key={box}/>
-                                        return a
-                                    })
-                                }
+                                {bbox.map(box => {
+                                    const a = (
+                                        <li
+                                            className="border border-black absolute"
+                                            style={{
+                                                left: `${
+                                                    bounding.left +
+                                                    box[0] * bounding.width
+                                                }px`,
+                                                top: `${
+                                                    bounding.top +
+                                                    box[1] * bounding.height
+                                                }px`,
+                                                right: `${
+                                                    bounding.right +
+                                                    (1 - box[2]) *
+                                                        bounding.width
+                                                }px`,
+                                                bottom: `${
+                                                    bounding.bottom +
+                                                    (1 - box[3]) *
+                                                        bounding.height
+                                                }px`,
+                                            }}
+                                            key={box}
+                                        />
+                                    )
+                                    return a
+                                })}
                             </ul>
-                        }
+                        )}
                     </div>
-                    <div className='flex gap-x-5 mt-3 left-0 bottom-0 m-auto self-center'>
+                    <div className="flex gap-x-5 justify-center">
                         <AiOutlineCloudUpload
                             size="2em"
                             onClick={handleSubmit}
                         />
-                        <GrClear size="2em" onClick={() => {
+                        <GrClear
+                            size="2em"
+                            onClick={() => {
                                 setTargetImage(null)
                                 setPredictedClasses([])
                                 setBbox([])
                                 setDetected(false)
-                            }
-                        }/>
+                            }}
+                        />
                     </div>
                 </div>
             )}
