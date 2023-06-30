818c9d0ad24148bb76fc66a74a3f42aa6a156258
needl3 <57314527+needl3@users.noreply.github.com>
2023-06-30 10:50:59 +0545

    Initialize Routes with Nav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,16 @@
-function App() {
-    return <div>Hello World</div>
-}
+import { Route, Routes } from "react-router-dom"
+import Nav from './components/Nav'
 
-export default App
+export default function App() {
+    return (
+        <>
+            <Nav/>
+            <Routes>
+                <Route path='/' element={<>Home</>}/>
+                <Route path='/image' element={<>Image</>}/>
+                <Route path='/video' element={<>Video</>}/>
+                <Route path='/info' element={<>Info</>}/>
+            </Routes>
+        </>
+    )
+}
