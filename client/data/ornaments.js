874c10df0e20f4714c6260b7fbfe5f4242e3e3d0
bimarshak7 <khanalbimarsha7@gmail.com>
2023-06-30 11:44:51 +0545

    Add descrition for dress and ornaments

---
+++ client/data/ornaments.js
@@ -0,0 +1,74 @@
+ornaments = {
+    0: {
+        name: 'Hāku patāsi',
+        culture: 'Newari',
+        description:
+            'Haku Patasi is made of local household cotton grown in different places surrounding Kathmandu valley.  Haku patasi is black sari with red borders worn by Nepali Newari women living in and around Kathmandu valley. Haku means Black and patasi meaning Sari in Newari language. Haku patasi (black sari) is very different than regular saris and dhoties worn by Nepali females. Haku patasi is usually heavier, and always black sari with red boarder. Haku Patasi is worn without wearing a petticoat (skirt worn inside the sari). This sari is worn from the waist to the calves and does not cover ankles as generally other saris do. Newar ladies and Newar girls living in Kathmandu valley and its vicinity wrap Haku Patasi around their waist and tie Patuka like a belt.',
+    },
+    1: {
+        name: 'Nyapu Sikha',
+        culture: 'Newari',
+        description:
+            '“Nyapu Shikha” is a chain with net like configuration worn on one side of the head. Nyapu in Newari means five and Shikha is chain. The jewelry has five rows of chains thus has the name. The jewelry is gold plated and handmade in Nepal. It usually use by the women & girls of Newari community.',
+    },
+    2: {
+        name: 'Tapalan',
+        culture: 'Newari',
+        description:
+            "Men's common costume consists of a long shirt called tapālan (Nepal Bhasa: तपालं) and tight fitting trousers known as suruwā (सुरुवा:). A waistcoat and coat may be worn over the shirt. Today, the garment is worn during special occasions, official functions and festivals while it is still everyday wear for many among the older generation. Men dress in tapālan and suruwā and women wear the hāku patāsi when taking part in the New Year's Day parade of Nepal Sambat. This traditional dress is also worn during wedding processions.",
+    },
+    3: {
+        name: 'Haku Tapli',
+        culture: 'Newari',
+        descrition:
+            'Haku Tapli is a traditional hat worn by the Newari male. Haku means "Black" and "Tapli" means "Topi" or "Hat" in Newari hence black hat. It is the newari version of popular nepali  traditional hat "Dhaka Topi"',
+    },
+    4: {
+        name: 'Hansuli',
+        culture: 'Tharu',
+        description:
+            ' Hansuli were worn in different parts of Nepal and north India, including in the northwest region of Humla. Our piece is based on a design popular within the Tharu community of southern Nepal. It is typically a heavy ornament made of solid silver or a metal composite',
+    },
+    5: {
+        name: 'Mangiya',
+        culture: 'Tharu',
+        description: '',
+    },
+    6: {
+        name: 'Ali Band',
+        culture: 'Tharu',
+        description:
+            'The hair ornament called ali band is popular with many different groups. It is a metal band that runs from the centre of the forehead over the part of the hair and around the sides of the head. The Tharu version is large with a prominent centre design and may not have the centre band. It looks similar to Sirbandi but has silver colour mostly.',
+    },
+    7: {
+        name: 'Bijaith',
+        culture: 'Tharu',
+        description: '',
+    },
+    8: {
+        name: 'Tharu Waist Chain',
+        culture: 'Tharu',
+        description: '',
+    },
+    9: {
+        name: 'Tharu Head Cloth',
+        culture: 'Tharu',
+        description: '',
+    },
+    10: {
+        name: 'Kanthula',
+        culture: 'Tharu',
+        description:
+            'A necklace of silver coins is called kanthula. The Tharu use silver coins not only in jewelry but as decorations of garments. What is interesting that sometimes Tharu men too wear coin necklaces.',
+    },
+    11: {
+        name: 'Tharu Broad Wrist Chain',
+        culture: 'Tharu',
+        description: '',
+    },
+    12: {
+        name: 'Sirbandi',
+        culture: 'Brahmin/Chhetri',
+        description: '',
+    },
+}
