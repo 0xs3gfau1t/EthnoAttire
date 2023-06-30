e176984178666e27900dda1c512e6cff6895c4da
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2023-07-01 04:43:51 +0545

    Update Description

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,13 +4,21 @@ import { BsFillArrowRightCircleFill } from 'react-icons/bs'
 const HomePage = () => {
     return (
         <div
-            className="h-full w-full flex flex-col items-center"
+            className="h-full w-full flex flex-col items-center gap-2"
             style={{ fontFamily: "'Exo 2', sans-serif" }}
         >
             <h1 className="text-4xl">ETHNO ATTIRE</h1>
             <img src="/home.gif" className="rounded-2xl" />
-            <h2 className="text-2xl mb-8">Identify cultures in a whimp</h2>
-            <h3 className="mb-8">Description Here</h3>
+            <h2 className="text-2xl font-semibold">
+                Identify cultures in a whimp
+            </h2>
+            <h3 className="text-justify px-4">
+                Designed to preserve the rich cultural heritage of Nepal, our
+                app, EthnoAttire allows users to explore and discover the
+                vibrant world of Nepali culture. Simply upload a photo or video,
+                and EthnoAttire will instantly identify the cultural items in
+                the media. Also, providing detailed information about the items.
+            </h3>
             <Link
                 to={'/image'}
                 className="flex flex-col items-center justify-center"
