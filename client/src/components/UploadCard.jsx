c2adea8f5cd954955fb8d0e1d0c9dfd4dd0370da
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2023-07-01 04:50:41 +0545

    update color

--- client/src/components/UploadCard.jsx
+++ client/src/components/UploadCard.jsx
@@ -11,14 +11,19 @@ export default function UploadCard({ handleChange, image, accept }) {
                 id="imageUpload"
                 onChange={e => handleChange(e)}
             />
-            <label htmlFor="imageUpload" className="h-1/2 w-full">
-                <div className="m-auto w-full relative">
-                    <img src={image} />
-                    <AiOutlinePlusCircle
-                        className="group-hover:fill-inherit transition duration-300 absolute m-auto left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-[7rem]"
-                        color="#444"
-                    />
-                </div>
+            <label
+                htmlFor="imageUpload"
+                className="h-1/2 w-full flex flex-col items-center"
+            >
+                <img src={image} />
+                <button
+                    className={`${accept.includes('video')
+                            ? 'bg-[#d6a5de]'
+                            : 'bg-blue-100'
+                        } px-4 py-2 rounded-md`}
+                >
+                    Upload
+                </button>
             </label>
         </div>
     )
